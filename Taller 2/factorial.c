02242a2b6e027d7378c1a3ba273a95a30ffdae67
Bojorge <bojorgearayamanuel@gmail.com>
2024-03-19 23:27:17 -0600

    Timer

--- Taller 2/factorial.c
+++ Taller 2/factorial.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <time.h>
 
 // Función recursiva para calcular el factorial
-unsigned long long factorial(unsigned long long n) {
-    printf("C");
+unsigned long long factorial(unsigned int n) {
     if (n == 0 || n == 1)
         return 1;
     else
@@ -10,11 +10,23 @@ unsigned long long factorial(unsigned long long n) {
 }
 
 int main() {
-    int num = 100;
-    unsigned long long fact;
+    unsigned int num;
+    printf("Ingrese un número entero: ");
+    scanf("%u", &num);
+    
+    // Inicia el temporizador
+    clock_t start = clock();
 
+    unsigned long long fact;
     fact = factorial(num);
-    printf("El factorial de %d es: %llu\n", num, fact);
+    // Detiene el temporizador
+    clock_t end = clock();
+    
+    // Calcula el tiempo transcurrido
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("El factorial de %d es %llu\n", num, fact);
+    printf("Tiempo de ejecución: %.9f segundos\n", time_spent);
 
     return 0;
 }
--- Taller 2/factorial.py
+++ Taller 2/factorial.py
@@ -1,9 +1,10 @@
 import sys
+import time
+
 sys.setrecursionlimit(10000)
 
 # Función recursiva para calcular el factorial
 def factorial(n):
-    print("Python")
     if n == 0 or n == 1:
         return 1
     else:
@@ -11,10 +12,21 @@ def factorial(n):
 
 # Función principal
 def main():
-    num = 1000
+    num = int(input("Ingrese un número entero: "))
+
+    # Inicia el temporizador
+    start_time = time.time()
 
     fact = factorial(num)
-    print(f"El factorial de {num} es: {fact}")
+    # Detiene el temporizador
+    end_time = time.time()
+
+    # Calcula el tiempo transcurrido
+    elapsed_time = end_time - start_time
+
+    print(f"El factorial de {num} es {fact}")
+    print(f"Tiempo de ejecución: {elapsed_time:.9f} segundos")
 
 if __name__ == "__main__":
     main()
+
